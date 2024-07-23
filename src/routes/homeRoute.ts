64beff3625c4dbfee688ae03f9550fad0989be0a
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-07-23 00:58:24 -0300

    feat: add setup project

---
+++ src/routes/homeRoute.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import HomeController from '../controllers/HomeController';
+
+const homeRoute: Router = Router();
+
+homeRoute.get('/', HomeController.index);
+
+export { homeRoute };
