4cf6bd4da9644d556408466a00166e2cf3752a2c
Edson Lima <111332935+edsuuu@users.noreply.github.com>
2024-07-28 18:33:08 -0300

    feat: add config database sequelize

---
+++ src/config/sequelize.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+require('dotenv').config();
+
+module.exports = {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASENAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+};
